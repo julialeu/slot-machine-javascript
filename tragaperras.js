e8c1e414e20d14d8a9d959637a9ce06123324540
Julia Leuenberger <leuenberger2101@gmail.com>
2021-11-29 23:33:33 +0100

    The machine can change the three images randomly

--- tragaperras.js
+++ tragaperras.js
@@ -1,5 +1,8 @@
-let numeroMonedasActual = 0;
+var listaImagenes = ["aubergine", "banana", "carrots", "cherries",
+    "dollar", "lemon", "orange", "peach",
+    "potato", "tomato"];
 
+let numeroMonedasActual = 0;
 
 function introducirMonedas() {
     let botonIntroducirElement = document.getElementById('botonIntroducirMonedas');
@@ -37,3 +40,48 @@ function añadirMensaje(mensajeTexto) {
 
     historialElement.appendChild(mensajeElement);
 }
+
+function bajarPalanca() {
+    if (numeroMonedasActual === 0) {
+        alert('Por favor, introduce monedas.');
+        return;
+    }
+    numeroMonedasActual = numeroMonedasActual - 1;
+    document.getElementById('numMonedasActuales').innerHTML = numeroMonedasActual;
+
+    if (numeroMonedasActual === 0) {
+        document.getElementById('botonIntroducirMonedas').disabled = false;
+    }
+
+    añadirMensaje('Gastas una moneda');
+
+    console.log('palancaDown')
+    let palancaImageElement = document.getElementById('palanca');
+    palancaImageElement.src = "/img/palancaDOWN.png";
+}
+
+function subirPalanca() {
+    let palancaImageElement = document.getElementById('palanca');
+    palancaImageElement.src = "/img/palancaUP.png";
+    let aleatorio1 = Math.floor(Math.random()*listaImagenes.length);
+    let fruta1 = listaImagenes[aleatorio1];
+    console.log(fruta1);
+    let imagen1 = document.getElementById('imagen1');
+    imagen1.src = "/img/" + fruta1 + ".png";
+
+    let aleatorio2 = Math.floor(Math.random()*listaImagenes.length);
+    let fruta2 = listaImagenes[aleatorio2];
+    console.log(fruta2);
+    let imagen2 = document.getElementById('imagen2');
+    imagen2.src = "/img/" + fruta2 + ".png";
+
+    let aleatorio3 = Math.floor(Math.random()*listaImagenes.length);
+    let fruta3 = listaImagenes[aleatorio3];
+    console.log(fruta3);
+    let imagen3 = document.getElementById('imagen3');
+    imagen3.src = "/img/" + fruta3 + ".png";
+
+
+}
+
+
